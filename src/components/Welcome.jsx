d478d1cf1153ecd7b46ae2a51767f312bc49d233
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-09-09 14:17:47 -0300

    welcome style created

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,15 @@
 import quiz from '../assets/quiz.svg'
+import { WelcomeContainer } from './Welcome.styles'
 
 export function Welcome(){
   return(
+    <WelcomeContainer>
       <div id='welcome'>
         <h2>Seja bem-vindo</h2>
         <p>Clique no botão abaixo para começar</p>
         <button>Iniciar</button>
         <img src={quiz} alt="início do quiz" />
       </div>
+    </WelcomeContainer>  
   )
 }
---
+++ src/components/Welcome.styles.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const WelcomeContainer = styled.div`
+  #welcome{
+    max-width: 500px
+  }
+
+  #welcome h2, p{
+    margin-bottom: 1rem;
+  }
+
+  #welcome p{
+    color: #8435de
+  }
+
+  #welcome img{
+    margin-top: 2rem;
+  }
+`;
