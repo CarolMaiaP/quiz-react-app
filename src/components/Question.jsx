0438446a389a13b1035a2d8564e2f129b1f12410
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-09-12 17:04:58 -0300

    question displayed on screen

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,13 +3,14 @@ import { QuizContext } from "../contexts/QuizContext"
 
 export function Question(){
   const [quizState, dispatch] = useContext(QuizContext);
+  const currentQuestion = quizState.questions[quizState.currentQuestion]
 
   console.log(quizState)
 
   return(
     <div id="question">
       <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
-      <h2>Pergunta atual</h2>
+      <h2>{currentQuestion.question}</h2>
       <div id="options-container">
         <p>Opções</p>
       </div>
