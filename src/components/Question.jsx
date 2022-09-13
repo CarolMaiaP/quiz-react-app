288e0df91f123684538d53da60056ac41137dc12
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-09-13 09:47:28 -0300

    question progression added

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -17,7 +17,7 @@ export function Question(){
         <div id="options-container">
           <p>Opções</p>
         </div>
-        <button>Continuar</button>
+        <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>Continuar</button>
       </div>
     </QuestionsContainer>
   )
--- src/contexts/QuizContext.jsx
+++ src/contexts/QuizContext.jsx
@@ -24,7 +24,19 @@ function quizReducer(state, action){
       const reorderedQuestions = questions.sort(() => {
         return Math.random() - 0.5;
       })
-      return state;  
+
+      return {
+        ...state,
+        questions: reorderedQuestions,
+      };  
+
+    case "CHANGE_QUESTION":
+      const nextQuestion = state.currentQuestion + 1;
+
+      return {
+        ...state,
+        currentQuestion: nextQuestion
+      }
 
     default: 
       return state
